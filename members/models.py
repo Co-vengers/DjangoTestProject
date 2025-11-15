fd2f906c5a0ae8528c431c46b9affef66f3df546
Co-vengers <vyomrohila@gmail.com>
2025-11-15 12:28:59 +0530

    Updated: phone number field

--- members/models.py
+++ members/models.py
@@ -1,10 +1,15 @@
 from django.db import models
+from django.core.validators import RegexValidator
 
 # Create your models here.
 class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
-    phone = models.IntegerField(null=True)
+    phone = models.IntegerField(
+        max_length=15, 
+        null=True,
+        validators=[RegexValidator(r'^\+?\d{10,15}$', 'Enter a valid phone number')]
+    )
     joined_date = models.DateField(null=True)
 
     def __str__(self):
